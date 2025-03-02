0312d178489dadf256d5947900d9517aa0ba016f
chinweikemichael <infinity_michael@yahoo.com>
2025-03-02 14:24:28 +0100

    featL implemented transaction screen functionalities

--- src/components/transactions/TransactionItem.tsx
+++ src/components/transactions/TransactionItem.tsx
@@ -3,11 +3,16 @@ import {View, StyleSheet, TouchableOpacity} from 'react-native';
 import {useThemedStyles} from '@/libs/hooks';
 import {Theme} from '@/libs/config/theme';
 import {Typography} from '@/components/common';
+import {NAIRA_SYMBOL} from '@/libs/constants';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {MainStackScreens} from '@/navigation/type';
+import {useNavigation} from '@react-navigation/native';
 
 type TransactionItemProps = {
   date: string;
   transRef: string;
-  amount: string;
+  amount: string | number;
+  loadStatus?: any;
   status: 'SUCCESSFUL' | 'FAILED' | 'PENDING';
 };
 
@@ -16,8 +21,12 @@ export const TransactionItem: React.FC<TransactionItemProps> = ({
   transRef,
   amount,
   status,
+  loadStatus,
 }) => {
   const style = useThemedStyles(styles);
+
+  const navigation =
+    useNavigation<NativeStackNavigationProp<MainStackScreens>>();
 
   // Determine status color
   const statusColor =
@@ -27,8 +36,25 @@ export const TransactionItem: React.FC<TransactionItemProps> = ({
       ? style.failedText
       : style.pendingText;
 
+  const loadStatusColor =
+    loadStatus === 'SUCCESSFUL'
+      ? style.successText
+      : loadStatus === 'FAILED'
+      ? style.failedText
+      : style.pendingText;
+
+  const viewDetails = () => {
+    navigation.navigate('TransactionDetails', {
+      date,
+      transRef,
+      amount,
+      status,
+      loadStatus,
+    });
+  };
+
   return (
-    <TouchableOpacity style={style.container}>
+    <TouchableOpacity onPress={viewDetails} style={style.container}>
       <View style={style.row}>
         <Typography style={style.label}>Date:</Typography>
         <Typography style={style.value}>{date}</Typography>
@@ -39,11 +65,20 @@ export const TransactionItem: React.FC<TransactionItemProps> = ({
       </View>
       <View style={style.row}>
         <Typography style={style.label}>Amount:</Typography>
-        <Typography style={style.value}>{amount}</Typography>
+        <Typography style={style.value}>
+          {NAIRA_SYMBOL}
+          {amount?.toLocaleString()}
+        </Typography>
       </View>
       <View style={style.row}>
         <Typography style={style.label}>Status:</Typography>
         <Typography style={[style.value, statusColor]}>{status}</Typography>
+      </View>
+      <View style={style.row}>
+        <Typography style={style.label}>Loading Progress:</Typography>
+        <Typography style={[style.value, loadStatusColor]}>
+          {loadStatus}
+        </Typography>
       </View>
     </TouchableOpacity>
   );
