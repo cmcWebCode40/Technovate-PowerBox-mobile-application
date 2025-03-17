23c0a42a0662cc182c89bba1a5b01640ec5bd939
chinweikemichael <infinity_michael@yahoo.com>
2025-03-17 11:14:26 +0100

    feat: added app native module  >> App app icons and splash screen setup for android

--- android/app/src/main/java/com/technovatepowerbox/MainActivity.kt
+++ android/app/src/main/java/com/technovatepowerbox/MainActivity.kt
@@ -1,11 +1,9 @@
-package com.smartpowerbox
+package com.technovatepowerbox
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
-import android.os.Bundle
-import org.devio.rn.splashscreen.SplashScreen
 
 class MainActivity : ReactActivity() {
 
@@ -13,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "SmartPowerBox"
+  override fun getMainComponentName(): String = "TechnovatePowerBox"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
@@ -21,9 +19,4 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
-
-  override fun onCreate(savedInstanceState: Bundle?) {
-    SplashScreen.show(this)
-    super.onCreate(null)
-  }
 }
--- android/app/src/main/java/com/technovatepowerbox/MainApplication.kt
+++ android/app/src/main/java/com/technovatepowerbox/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.smartpowerbox
+package com.technovatepowerbox
 
 import android.app.Application
 import com.facebook.react.PackageList
@@ -9,8 +9,10 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
-import com.smartpowerbox.MqttPackage
+// import com.technovatepowerbox.MqttPackage
+import com.technovatepowerbox.NativeLocalStoragePackage
 
 class MainApplication : Application(), ReactApplication {
 
@@ -20,7 +22,8 @@ class MainApplication : Application(), ReactApplication {
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
-               add(MqttPackage())
+              // add(MqttPackage())
+              add(NativeLocalStoragePackage())
             }
 
         override fun getJSMainModuleName(): String = "index"
@@ -36,7 +39,7 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, false)
+    SoLoader.init(this, OpenSourceMergedSoMapping)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
