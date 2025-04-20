ea49466b9e1f7e08f9d94be85e977afea16c4d86
chinweikemichael <infinity_michael@yahoo.com>
2025-04-20 02:20:53 +0100

    feat: setup ios new architechure + BLE . support

---
+++ ios/NativeLocalStorage/RCTNativeLocalStorage.h
@@ -0,0 +1,16 @@
+//
+//  RCTNativeLocalStorage.h
+//  TechnovatePowerBox
+//
+//  Created by Chinweike Michael on 01/04/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCTNativeLocalStorage : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/NativeLocalStorage/RCTNativeLocalStorage.m
@@ -0,0 +1,12 @@
+//
+//  RCTNativeLocalStorage.m
+//  TechnovatePowerBox
+//
+//  Created by Chinweike Michael on 01/04/2025.
+//
+
+#import "RCTNativeLocalStorage.h"
+
+@implementation RCTNativeLocalStorage
+
+@end
