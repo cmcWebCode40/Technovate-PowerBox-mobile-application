c3e28fcdff49719f9d2af1688f0e2120ce6faf39
chinweikemichael <infinity_michael@yahoo.com>
2025-04-26 23:37:26 +0100

    chore: updated real time data data types

--- src/libs/mqtt/types.ts
+++ src/libs/mqtt/types.ts
@@ -6,6 +6,8 @@ export interface DeviceRealTimeInfo {
   cell4: number;
   acVolt: number;
   balUnit: number;
+  power: number;
+  frequency: number;
   solarVoltage: number;
   solarCurrent: number;
   acOut: number;
@@ -38,27 +40,3 @@ export interface PublishResponse {
     msg: string
     id: string
 }
-
-
-const tt = {
-  'battVolt': 53.07,
-  'battPercent': 96,
-  'balUnit': 4.887,
-  'totalUnit': 5,
-  'battHealth': 100,
-  'battRCC': 96.27,
-  'chargeCycles': 1,
-  'battFCC': 99.98,
-  'solarVoltage': 0,
-  'solarCurrent': 0,
-  'acVolt': 230.3,
-  'power': 123.3,
-  'usage': 0.113,
-  'powerFactor': 1,
-  'frequency': 50,
-  'chargeCurrent': -3.48,
-  'state': 'on',
-  'mode': 'prepaid',
-  'deviceId': '5100760000001',
-  'deviceState': 'discharging',
-};
